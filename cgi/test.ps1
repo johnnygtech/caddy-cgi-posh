94f730bdb09b631b70f77406573bb8bd26f42374
John Gleason <john@gleason.technology>
2020-09-12 09:07:02 -0400

    clean up sample scripts

--- cgi/test.ps1
+++ cgi/test.ps1
@@ -1,6 +1,6 @@
 #!C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -File
 $output += "Content-Type: text/plain`n`n"
-$output += "Hello world`n`n"
+$output += "Hello world"
 #$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 write-output $output
 #Get-date
