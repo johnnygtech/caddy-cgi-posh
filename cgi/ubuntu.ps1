b429cb3c9b60a0c9083cc1f027f700d6e4242b4a
John Gleason <john@gleason.technology>
2020-09-12 07:56:56 -0400

    setup different testing combinations in caddyfiles

--- cgi/ubuntu.ps1
+++ cgi/ubuntu.ps1
@@ -1,6 +1,7 @@
+#!/usr/bin/env pwsh
 
 $output += "Content-Type: text/plain`n`n"
-$output += "Hello world`n`n"
+$output += "Hello world"
 #$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 write-output $output
 #Get-date
