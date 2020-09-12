88dc8e46e3cc1136551b390a4372d679d5f09660
John Gleason <john@gleason.technology>
2020-09-12 08:35:17 -0400

    working windows wildcard path matching

--- cgi/test.ps1
+++ cgi/test.ps1
@@ -1,3 +1,4 @@
+#!C:\WINDOWS\System32\WindowsPowerShell\v1.0\powershell.exe -File
 $output += "Content-Type: text/plain`n`n"
 $output += "Hello world`n`n"
 #$PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
