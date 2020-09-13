06d7a219ded4486761646891b7ef8db0b3f4e105
John Gleason <john@gleason.technology>
2020-09-13 14:26:37 -0400

    add splatter application and reorganize examples

---
+++ cgi/apps/splatter.ps1
@@ -0,0 +1,113 @@
+#!/usr/bin/env pwsh
+
+#use this function if psversion -lt 6?
+function ConvertTo-Hashtable
+{
+#http://stackoverflow.com/questions/22002748/hashtables-from-convertfrom-json-have-different-type-from-powershells-built-in-h
+    param (
+        [Parameter(ValueFromPipeline)]
+        $InputObject
+    )
+    process
+    {
+        if ($null -eq $InputObject) { return $null }
+        if ($InputObject -is [System.Collections.IEnumerable] -and $InputObject -isnot [string])
+        {
+            $collection = @(
+                foreach ($object in $InputObject) { ConvertTo-Hashtable $object }
+            )
+            Write-Output -NoEnumerate $collection
+        }
+        elseif ($InputObject -is [psobject])
+        {
+            $hash = @{}
+            foreach ($property in $InputObject.PSObject.Properties)
+            {
+                $hash[$property.name] = ConvertTo-Hashtable $property.Value
+            }
+            $hash
+        }
+        else
+        {
+            $InputObject
+        }
+    }
+}
+
+$default="Content-Type: text/html`n`n
+<html>
+    <body>
+        <h1>Splatter</h1>
+        <p>Splatter is a powershell handler for posh-caddy that combines http query string or post data to form and execute a powershell command and return the results</p>
+        <a href=`"http://localhost:9002/apps/splatter?command=get-date`">Example: Get-Date</a>
+    </body>
+</html>
+"     
+
+#handle request methods
+switch($env:REQUEST_METHOD)
+{
+    "GET"
+        {
+            Write-verbose "Method is GET: Check query string"
+            if(-not $([string]::isnullorempty($env:QUERY_STRING))) #user gave us something to do
+            {
+                #force content type, this is a decision to always return json
+                $contentType="Content-Type: application/json`n`n"
+
+                #split
+                $queryStringSplit=$($env:QUERY_STRING -split "&")
+                $queryStringHashTable=@{} #hash table to easily handle paramters
+                $command=$null
+                $queryStringSplit | %{ #build hash table
+                    $entry = $_ -split "=" 
+                    if($entry[0] -eq "command"){$command = $entry[1]}else #look for command entry and don't add it to hashtablea
+                    {$queryStringHashTable.Add($entry[0],$($entry[1]))} #hash table is for splatting!
+                }
+                if(-not $command)
+                {
+                    return "$contentType$(Convertto-json @{"responsecode"="404";"reponse"="command not provided"})"
+                }
+                else
+                {
+                    Write-Verbose "Executing Command: $Command `r`n"
+                    return "$contentType$(convertto-json $(& $command @queryStringHashTable))"
+                }
+            }
+            else  #return standard splash page
+            {
+                return $default
+            }
+        }
+    "POST"
+        {
+            Write-Verbose "Method is POST: check post data from std in"
+            if(-not $([string]::isnullorempty($input))) #user gave us something to do
+            {
+                #force content type, this is a decision to always return json
+                $contentType="Content-Type: application/json`n`n"
+
+                $PostDataHashTable=$(ConvertFrom-Json $input) | ConvertTo-HashTable
+                $command=$null
+
+                #Extract COmmand
+                $command = $PostDataHashTable."command"
+                if(-not $command)
+                {
+                    return "$contentType$(Convertto-json @{"responsecode"="404";"reponse"="command not provided"})"
+                }
+                else
+                {
+                    $PostDataHashTable.remove("command");
+                    return "$contentType$(convertto-json $(& $command @PostDataHashTable))"
+                }
+                return "$contentType$input"
+            }
+            else #return standard splash page
+            {
+                return $default
+            }
+        }
+    default { return Convertto-json @{"responsecode"="404";"reponse"="method not found: $env:REQUEST_METHOD"} }
+}
+
--- cgi/examples/getenv.ps1
+++ cgi/examples/getenv.ps1
@@ -1,4 +1,4 @@
 #!/usr/bin/env pwsh
-$contentType="Content-Type: text/plain`n`n"
+$contentType="Content-Type: application/json`n`n"
 $body=$(convertto-json $(get-item env:))
 return "$contentType$body"
