db55854fba931496baca20c804f0b4eb00dd2c40
John Gleason <john@gleason.technology>
2020-09-15 10:24:10 -0400

    Posh == 9054 cause i'm a child

--- tests.ps1
+++ tests.ps1
@@ -6,10 +6,10 @@ $greenCheck = @{
 
 $expected="Hello world"
 
-if($($(Invoke-WebRequest http://localhost:9002/examples/test | select -ExpandProperty content) -match $expected)){write-host @greencheck}
-if($($(Invoke-WebRequest http://localhost:9002/examples/test.ps1 | select -ExpandProperty content) -match $expected)){write-host @greencheck}
+if($($(Invoke-WebRequest http://localhost:9054/examples/test | select -ExpandProperty content) -match $expected)){write-host @greencheck}
+if($($(Invoke-WebRequest http://localhost:9054/examples/test.ps1 | select -ExpandProperty content) -match $expected)){write-host @greencheck}
 
-if($($(Invoke-WebRequest "http://localhost:9002/examples/echo?test=hello world" | select -ExpandProperty content) -match $expected)){write-host @greencheck}
-if($($(Invoke-WebRequest http://localhost:9002/examples/echo.ps1 -method POST -body "Hello World" | select -ExpandProperty content) -match $expected)){write-host @greencheck}
+if($($(Invoke-WebRequest "http://localhost:9054/examples/echo?test=hello world" | select -ExpandProperty content) -match $expected)){write-host @greencheck}
+if($($(Invoke-WebRequest http://localhost:9054/examples/echo.ps1 -method POST -body "Hello World" | select -ExpandProperty content) -match $expected)){write-host @greencheck}
 
-if($(Invoke-RestMethod http://localhost:9002/examples/getenv | convertto-json -outvariable test)){write-host @greencheck}
+if($(Invoke-RestMethod http://localhost:9054/examples/getenv | convertto-json -outvariable test)){write-host @greencheck}
